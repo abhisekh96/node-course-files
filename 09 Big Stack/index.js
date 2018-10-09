50320f5a1664f03ab2acdd3e4cb45012ac3058ed
Abhisek Hota <hota.abhisek@gmail.com>
2018-10-10 01:09:09 +0530

    9 October 2018

--- 09 Big Stack/index.js
+++ 09 Big Stack/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
+
+// MongoDB configuration
+const db = require("./setup/myurl").mongoURL;
+
+// Connect to database
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => { console.log("MongoDB connected successfully.") })
+  .catch(err => { console.log(err) });
 
 const port = process.env.PORT || 3000;
 
---
+++ 09 Big Stack/setup/myurl.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURL: "mongodb://abhisekh:Abhi$ekh96@ds125423.mlab.com:25423/abhisekh",
+  secret: "mystrongsecret"
+}
